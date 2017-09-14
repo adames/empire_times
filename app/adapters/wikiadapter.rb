f1a1f96e2db618ddab483e35209ea5dc59f4288f
adames <adameshh@gmail.com>
2017-09-14 15:16:15 -0400

    switched results to 8

--- app/adapters/wikiadapter.rb
+++ app/adapters/wikiadapter.rb
@@ -27,7 +27,7 @@ class WikipediaAPI
       action: 'opensearch',
       profile: 'fuzzy',
       format: 'json',
-      limit: '5',
+      limit: '8',
       namespace: '0',
       redirect: 'resolve',
     }
